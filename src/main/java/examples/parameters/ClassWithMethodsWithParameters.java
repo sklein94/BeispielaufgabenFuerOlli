f26ef167ffeb34353b57f32f019b2e633aae6d18
sklein <simon.klein.sz.1@gmail.com>
2017-09-06 16:48:48 +0200

    Created Project and Folders

---
+++ src/main/java/examples/parameters/ClassWithMethodsWithParameters.java
@@ -0,0 +1,11 @@
+package examples.parameters;
+
+public class ClassWithMethodsWithParameters {
+
+    public void sayHello(int numberOfHellos){
+        for (int i = 0; i < numberOfHellos; i++){
+            System.out.println("Hello!");
+        }
+    }
+
+}
---
+++ src/main/java/examples/parameters/Main.java
@@ -0,0 +1,7 @@
+package examples.parameters;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
