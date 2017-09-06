d1cc4af142324f1a3fba0ce4d5710fb9219af7d7
sklein <simon.klein.sz.1@gmail.com>
2017-09-06 16:56:59 +0200

    16:56 06.09.2017 -> Sicherung

--- src/main/java/examples/parameters/ClassWithMethodsWithParameters.java
+++ src/main/java/examples/parameters/ClassWithMethodsWithParameters.java
@@ -2,9 +2,18 @@ package examples.parameters;
 
 public class ClassWithMethodsWithParameters {
 
-    public void sayHello(int numberOfHellos){
+    //Diese Methode enthält formale Parameter
+    //Diese Parameter sind nur innerhalb der Methode gültig.
+    public static void sayHello(int numberOfHellos){
         for (int i = 0; i < numberOfHellos; i++){
             System.out.println("Hello!");
+        }
+    }
+
+    //Diese Methode enthält todo
+    public static void saySomeWords(String words, int numberOfWords){
+        for (int i = 0; i < numberOfWords; i++){
+            System.out.println(words);
         }
     }
 
--- src/main/java/examples/parameters/Main.java
+++ src/main/java/examples/parameters/Main.java
@@ -2,6 +2,19 @@ package examples.parameters;
 
 public class Main {
     public static void main(String[] args){
+        //Beispiele für aktuelle Parameter (Innerhalb der runden Klammern)
 
+        //Aufruf mit einem Literal
+        ClassWithMethodsWithParameters.sayHello(5);
+
+        //Aufruf mit Variable
+        int numberOfHellos = 3;
+        ClassWithMethodsWithParameters.sayHello(numberOfHellos);
+
+        //Reihenfolge spielt eine Rolle
+        ClassWithMethodsWithParameters.saySomeWords("Wie gehts?", 5);
+
+        //Falsch ist:
+        //ClassWithMethodsWithParameters.saySomeWords(5, "Wie gehts?");
     }
 }
